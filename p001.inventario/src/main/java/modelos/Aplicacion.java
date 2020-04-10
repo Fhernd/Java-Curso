e5ac143bff0820f60463095ef8e5d7e3bbfd6865
infzer0 <johnortizo@outlook.com>
2020-04-10 12:42:37 -0500

    Java - Curso - Menús de Operaciones
    Java Curso: 23 Crear Métodos para Mostrar Menús de Selección de Operaciones para el Usuario.

--- p001.inventario/src/main/java/modelos/Aplicacion.java
+++ p001.inventario/src/main/java/modelos/Aplicacion.java
@@ -8,6 +8,33 @@ public class Aplicacion {
 
 	public static void main(String[] args) {
 		teclado = new Scanner(System.in);
+		
+		
+	}
+	
+	public static void mostrarMenuPrincipal() {
+		System.out.println("=== MENÚ PRINCIPAL ===");
+		System.out.println("1. Gestión Clientes");
+		System.out.println("2. Gestión Proveedores");
+		System.out.println("3. Gestión Productos");
+		System.out.println("4. Gestión Facturación");
+		System.out.println("0. Salir");
+	}
+	
+	public static void mostrarSubmenu(String tipoMenu) {
+		System.out.printf("*** Menú Gestión %s***\n", tipoMenu);
+		System.out.println("1. Crear");
+		System.out.println("2. Buscar");
+		System.out.println("3. Actualizar");
+		System.out.println("4. Eliminar");
+		System.out.println("0. Salir");
+	}
+	
+	public static void mostrarSubmenuFacturacion() {
+		System.out.println("*** Menú Gestión Facturación ***");
+		System.out.println("1. Crear");
+		System.out.println("2. Buscar");
+		System.out.println("0. Salir");
 	}
 	
 	public static String capturarCadenaCaracteres(String mensaje) {
