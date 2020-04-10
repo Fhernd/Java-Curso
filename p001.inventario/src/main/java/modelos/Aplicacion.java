d78527ca308ce8e71d96c4629c21f9a0c16c80aa
infzer0 <johnortizo@outlook.com>
2020-04-10 12:39:56 -0500

    Java - Curso - Métodos para Capturar Reales y Cadenas de Caracteres
    Java Curso: 23 Crear Métodos para Mostrar Menús de Selección de Operaciones para el Usuario.

--- p001.inventario/src/main/java/modelos/Aplicacion.java
+++ p001.inventario/src/main/java/modelos/Aplicacion.java
@@ -9,14 +9,39 @@ public class Aplicacion {
 	public static void main(String[] args) {
 		teclado = new Scanner(System.in);
 	}
+	
+	public static String capturarCadenaCaracteres(String mensaje) {
+		String resultado;
+		
+		while(true) {
+			resultado = teclado.nextLine().strip();
+			
+			if(!resultado.isEmpty()) {
+				return resultado;
+			}
+			
+			System.out.println("MENSAJE: Ha escrito una cadena vacía. Específique un valor concreto.");
+		}
+	}
 
-	public static int capturarEntero(String mensaje) {
+	public static int capturarNumeroEntero(String mensaje) {
 		while(true) {
 			try {
 				System.out.printf("%s: ", mensaje);
 				return Integer.parseInt(teclado.nextLine());
 			} catch(NumberFormatException e) {
 				System.out.println("MENSAJE: Digite un valor que corresponda con un número entero.");
+			}
+		}
+	}
+	
+	public static double capturarNumeroReal(String mensaje) {
+		while(true) {
+			try {
+				System.out.printf("%s: ", mensaje);
+				return Double.parseDouble(teclado.nextLine());
+			} catch(NumberFormatException e) {
+				System.out.println("MENSAJE: Digite un valor que corresponda con un número real.");
 			}
 		}
 	}
