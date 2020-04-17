30f09451937f15b80f3d41488468334ebc975398
infzer0 <johnortizo@outlook.com>
2020-04-17 09:17:06 -0500

    Java - Curso - new ActionListener()
    Java Curso: 91 Programar un Evento ActionEvent para Confirmar la Salida de la Aplicación.

--- p002.inventario.gui/src/main/java/p002/inventario/gui/Aplicacion.java
+++ p002.inventario.gui/src/main/java/p002/inventario/gui/Aplicacion.java
@@ -8,11 +8,15 @@ import java.awt.BorderLayout;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JDesktopPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Aplicacion {
 
-	private JFrame frame;
+	private static final int CODIGO_SALIDA_SATISFACTORIA = 0;
+	private JFrame frmSistemaDeGestion;
 
 	/**
 	 * Launch the application.
@@ -22,7 +26,7 @@ public class Aplicacion {
 			public void run() {
 				try {
 					Aplicacion window = new Aplicacion();
-					window.frame.setVisible(true);
+					window.frmSistemaDeGestion.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -41,81 +45,92 @@ public class Aplicacion {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 800, 600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+		frmSistemaDeGestion = new JFrame();
+		frmSistemaDeGestion.setTitle("Sistema de Gestión de Inventario - Ferretería Don Tuerca");
+		frmSistemaDeGestion.setBounds(100, 100, 800, 600);
+		frmSistemaDeGestion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 		JMenuBar menuBar = new JMenuBar();
-		frame.setJMenuBar(menuBar);
-		
+		frmSistemaDeGestion.setJMenuBar(menuBar);
+
 		JMenu mnuArchivo = new JMenu("Archivo");
 		menuBar.add(mnuArchivo);
-		
+
 		JMenuItem mniSalir = new JMenuItem("Salir");
+		mniSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int resultado = JOptionPane.showConfirmDialog(frmSistemaDeGestion, "¿Desea cerrar la aplicación?", "Confirmación",
+						JOptionPane.YES_NO_OPTION);
+				
+				if (resultado == JOptionPane.YES_OPTION) {
+					System.exit(CODIGO_SALIDA_SATISFACTORIA);
+				}
+			}
+		});
 		mnuArchivo.add(mniSalir);
-		
+
 		JMenu mnuClientes = new JMenu("Clientes");
 		menuBar.add(mnuClientes);
-		
+
 		JMenuItem mniClientesCrear = new JMenuItem("Crear");
 		mnuClientes.add(mniClientesCrear);
-		
+
 		JMenuItem mniClientesBuscar = new JMenuItem("Buscar");
 		mnuClientes.add(mniClientesBuscar);
-		
+
 		JMenuItem mniClientesActualizar = new JMenuItem("Actualizar");
 		mnuClientes.add(mniClientesActualizar);
-		
+
 		JMenuItem mniClientesEliminar = new JMenuItem("Eliminar");
 		mnuClientes.add(mniClientesEliminar);
-		
+
 		JMenu mnuProveedores = new JMenu("Proveedores");
 		menuBar.add(mnuProveedores);
-		
+
 		JMenuItem mniProveedoresCrear = new JMenuItem("Crear");
 		mnuProveedores.add(mniProveedoresCrear);
-		
+
 		JMenuItem mniProveedoresBuscar = new JMenuItem("Buscar");
 		mnuProveedores.add(mniProveedoresBuscar);
-		
+
 		JMenuItem mniProveedoresActualizar = new JMenuItem("Actualizar");
 		mnuProveedores.add(mniProveedoresActualizar);
-		
+
 		JMenuItem mniProveedoresEliminar = new JMenuItem("Eliminar");
 		mnuProveedores.add(mniProveedoresEliminar);
-		
+
 		JMenu mnuProductos = new JMenu("Productos");
 		menuBar.add(mnuProductos);
-		
+
 		JMenuItem mniProductosCrear = new JMenuItem("Crear");
 		mnuProductos.add(mniProductosCrear);
-		
+
 		JMenuItem mniProductosBuscar = new JMenuItem("Buscar");
 		mnuProductos.add(mniProductosBuscar);
-		
+
 		JMenuItem mniProductosActualizar = new JMenuItem("Actualizar");
 		mnuProductos.add(mniProductosActualizar);
-		
+
 		JMenuItem mniProductosEliminar = new JMenuItem("Eliminar");
 		mnuProductos.add(mniProductosEliminar);
-		
+
 		JMenu mnuFacturacion = new JMenu("Facturación");
 		menuBar.add(mnuFacturacion);
-		
+
 		JMenuItem mniFacturacionCrear = new JMenuItem("Crear");
 		mnuFacturacion.add(mniFacturacionCrear);
-		
+
 		JMenuItem mniFacturacionBuscar = new JMenuItem("Buscar");
 		mnuFacturacion.add(mniFacturacionBuscar);
-		
+
 		JMenu mnuAyuda = new JMenu("Ayuda");
 		menuBar.add(mnuAyuda);
-		
+
 		JMenuItem mniAcercaDe = new JMenuItem("Acerca de");
 		mnuAyuda.add(mniAcercaDe);
-		frame.getContentPane().setLayout(new BorderLayout(0, 0));
-		
+		frmSistemaDeGestion.getContentPane().setLayout(new BorderLayout(0, 0));
+
 		JDesktopPane desktopPane = new JDesktopPane();
-		frame.getContentPane().add(desktopPane, BorderLayout.CENTER);
+		frmSistemaDeGestion.getContentPane().add(desktopPane, BorderLayout.CENTER);
 	}
 }
