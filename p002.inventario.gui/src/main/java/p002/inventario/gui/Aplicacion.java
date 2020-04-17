401a2fb9c128ab9763f909f6699a2d39979a9423
infzer0 <johnortizo@outlook.com>
2020-04-17 06:15:58 -0500

    Java - Curso - Creación de un Formulario
    Java Curso: 86 Instalación del Diseñador Visual de Interfaces Gráficas de Usuario WindowBuilder.

---
+++ p002.inventario.gui/src/main/java/p002/inventario/gui/Aplicacion.java
@@ -0,0 +1,45 @@
+package p002.inventario.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+
+public class Aplicacion {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Aplicacion window = new Aplicacion();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Aplicacion() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+	}
+}
