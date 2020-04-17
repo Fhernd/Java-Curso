d92a4af854c98695b6041b38045bf6e3ced41290
infzer0 <johnortizo@outlook.com>
2020-04-17 07:14:22 -0500

    Java - Curso - Arrastrar un JDesktopPane.
    Java Curso: 88 Creación de la Barra de Menú y Configuración de un Contenedor JDesktopPane (MDI).

--- p002.inventario.gui/src/main/java/p002/inventario/gui/Aplicacion.java
+++ p002.inventario.gui/src/main/java/p002/inventario/gui/Aplicacion.java
@@ -5,6 +5,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.BorderLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JDesktopPane;
 
 public class Aplicacion {
 
@@ -38,8 +42,26 @@ public class Aplicacion {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnuArchivo = new JMenu("Archivo");
+		menuBar.add(mnuArchivo);
+		
+		JMenuItem mniSalir = new JMenuItem("Salir");
+		mnuArchivo.add(mniSalir);
+		
+		JMenu mnuAyuda = new JMenu("Ayuda");
+		menuBar.add(mnuAyuda);
+		
+		JMenuItem mniAcercaDe = new JMenuItem("Acerca de");
+		mnuAyuda.add(mniAcercaDe);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JDesktopPane desktopPane = new JDesktopPane();
+		frame.getContentPane().add(desktopPane, BorderLayout.CENTER);
 	}
 }
