cbdecf96088a8a4f31e29cb167a09faff53625a0
infzer0 <johnortizo@outlook.com>
2020-04-18 07:18:10 -0500

    Java - Curso - Más Componentes de GUI
    Java Curso: 93 Usar Componentes Visuales JLabel, JTextField y JButton para el Formulario de Crear Clientes.

--- p002.inventario.gui/src/main/java/p002/inventario/gui/ClientesCrearFormulario.java
+++ p002.inventario.gui/src/main/java/p002/inventario/gui/ClientesCrearFormulario.java
@@ -12,6 +12,7 @@ import com.jgoodies.forms.layout.RowSpec;
 import com.jgoodies.forms.layout.FormSpecs;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class ClientesCrearFormulario extends JInternalFrame {
 
@@ -19,7 +20,12 @@ public class ClientesCrearFormulario extends JInternalFrame {
 	 * Serial Version UID.
 	 */
 	private static final long serialVersionUID = 4454429000716379328L;
-	private JTextField textField;
+	private JTextField txtCedula;
+	private JTextField txtNombres;
+	private JTextField txtApellidos;
+	private JTextField txtTelefono;
+	private JTextField txtDireccion;
+	private JTextField txtCorreoElectronico;
 
 	/**
 	 * Launch the application.
@@ -45,10 +51,10 @@ public class ClientesCrearFormulario extends JInternalFrame {
 		setTitle("Clientes - Crear");
 		setBounds(100, 100, 450, 300);
 		
-		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(null, "Datos", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		getContentPane().add(panel, BorderLayout.CENTER);
-		panel.setLayout(new FormLayout(new ColumnSpec[] {
+		JPanel pnlClientesCrearDatos = new JPanel();
+		pnlClientesCrearDatos.setBorder(new TitledBorder(null, "Datos", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		getContentPane().add(pnlClientesCrearDatos, BorderLayout.CENTER);
+		pnlClientesCrearDatos.setLayout(new FormLayout(new ColumnSpec[] {
 				FormSpecs.RELATED_GAP_COLSPEC,
 				FormSpecs.DEFAULT_COLSPEC,
 				FormSpecs.RELATED_GAP_COLSPEC,
@@ -59,14 +65,69 @@ public class ClientesCrearFormulario extends JInternalFrame {
 				ColumnSpec.decode("default:grow"),},
 			new RowSpec[] {
 				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
 				FormSpecs.DEFAULT_ROWSPEC,}));
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		panel.add(lblNewLabel, "2, 2");
+		JLabel lblCedula = new JLabel("Cédula");
+		pnlClientesCrearDatos.add(lblCedula, "2, 2");
 		
-		textField = new JTextField();
-		panel.add(textField, "8, 2, fill, default");
-		textField.setColumns(10);
+		txtCedula = new JTextField();
+		pnlClientesCrearDatos.add(txtCedula, "8, 2, fill, default");
+		txtCedula.setColumns(10);
+		
+		JLabel lblNombres = new JLabel("Nombres");
+		pnlClientesCrearDatos.add(lblNombres, "2, 4");
+		
+		txtNombres = new JTextField();
+		pnlClientesCrearDatos.add(txtNombres, "8, 4, fill, default");
+		txtNombres.setColumns(10);
+		
+		JLabel lblApellidos = new JLabel("Apellidos");
+		pnlClientesCrearDatos.add(lblApellidos, "2, 6");
+		
+		txtApellidos = new JTextField();
+		pnlClientesCrearDatos.add(txtApellidos, "8, 6, fill, default");
+		txtApellidos.setColumns(10);
+		
+		JLabel lblTelefono = new JLabel("Teléfono");
+		pnlClientesCrearDatos.add(lblTelefono, "2, 8");
+		
+		txtTelefono = new JTextField();
+		pnlClientesCrearDatos.add(txtTelefono, "8, 8, fill, default");
+		txtTelefono.setColumns(10);
+		
+		JLabel lblDireccion = new JLabel("Dirección");
+		pnlClientesCrearDatos.add(lblDireccion, "2, 10");
+		
+		txtDireccion = new JTextField();
+		pnlClientesCrearDatos.add(txtDireccion, "8, 10, fill, default");
+		txtDireccion.setColumns(10);
+		
+		JLabel lblCorreoElectronico = new JLabel("Correo electrónico");
+		pnlClientesCrearDatos.add(lblCorreoElectronico, "2, 12");
+		
+		txtCorreoElectronico = new JTextField();
+		pnlClientesCrearDatos.add(txtCorreoElectronico, "8, 12, fill, default");
+		txtCorreoElectronico.setColumns(10);
+		
+		JButton btnCrear = new JButton("Crear");
+		pnlClientesCrearDatos.add(btnCrear, "8, 14");
+		
+		JButton btnLimpiar = new JButton("Limpiar");
+		pnlClientesCrearDatos.add(btnLimpiar, "8, 16");
 
 	}
 
