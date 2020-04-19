2ab29e0fc9d333ba28c5fc2bbace7d390458f3b0
infzer0 <johnortizo@outlook.com>
2020-04-19 16:40:51 -0500

    Update ClientesActualizarFormulario.java
    Java Curso: 107 Crear el Formulario para Actualizar los Datos de un Cliente a partir de su Cédula.

--- p002.inventario.gui/src/main/java/p002/inventario/gui/ClientesActualizarFormulario.java
+++ p002.inventario.gui/src/main/java/p002/inventario/gui/ClientesActualizarFormulario.java
@@ -14,9 +14,15 @@ import com.jgoodies.forms.layout.RowSpec;
 import com.jgoodies.forms.layout.FormSpecs;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class ClientesActualizarFormulario extends JInternalFrame {
-	private JTextField textField;
+	private JTextField txtCedula;
+	private JTextField txtNombres;
+	private JTextField txtApellidos;
+	private JTextField txtTelefono;
+	private JTextField txtDireccion;
+	private JTextField txtCorreoElectronico;
 
 	/**
 	 * Launch the application.
@@ -59,14 +65,74 @@ public class ClientesActualizarFormulario extends JInternalFrame {
 				ColumnSpec.decode("default:grow"),},
 			new RowSpec[] {
 				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
 				FormSpecs.DEFAULT_ROWSPEC,}));
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		pnlClientesActualizar.add(lblNewLabel, "2, 2");
+		JLabel lblCedula = new JLabel("Cédula");
+		pnlClientesActualizar.add(lblCedula, "2, 2");
 		
-		textField = new JTextField();
-		pnlClientesActualizar.add(textField, "12, 2, fill, default");
-		textField.setColumns(10);
+		txtCedula = new JTextField();
+		pnlClientesActualizar.add(txtCedula, "12, 2, fill, default");
+		txtCedula.setColumns(10);
+		
+		JButton btnBuscar = new JButton("Buscar");
+		pnlClientesActualizar.add(btnBuscar, "12, 4");
+		
+		JLabel lblNombres = new JLabel("Nombres");
+		pnlClientesActualizar.add(lblNombres, "2, 6");
+		
+		txtNombres = new JTextField();
+		txtNombres.setEditable(false);
+		pnlClientesActualizar.add(txtNombres, "12, 6, fill, default");
+		txtNombres.setColumns(10);
+		
+		JLabel lblApellidos = new JLabel("Apellidos");
+		pnlClientesActualizar.add(lblApellidos, "2, 8");
+		
+		txtApellidos = new JTextField();
+		txtApellidos.setEditable(false);
+		pnlClientesActualizar.add(txtApellidos, "12, 8, fill, default");
+		txtApellidos.setColumns(10);
+		
+		JLabel lblTelefono = new JLabel("Teléfono");
+		pnlClientesActualizar.add(lblTelefono, "2, 10");
+		
+		txtTelefono = new JTextField();
+		txtTelefono.setEditable(false);
+		pnlClientesActualizar.add(txtTelefono, "12, 10, fill, default");
+		txtTelefono.setColumns(10);
+		
+		JLabel lblDireccion = new JLabel("Dirección");
+		pnlClientesActualizar.add(lblDireccion, "2, 12");
+		
+		txtDireccion = new JTextField();
+		txtDireccion.setEditable(false);
+		pnlClientesActualizar.add(txtDireccion, "12, 12, fill, default");
+		txtDireccion.setColumns(10);
+		
+		JLabel lblCorreoElectronico = new JLabel("New label");
+		pnlClientesActualizar.add(lblCorreoElectronico, "2, 14");
+		
+		txtCorreoElectronico = new JTextField();
+		txtCorreoElectronico.setEditable(false);
+		pnlClientesActualizar.add(txtCorreoElectronico, "12, 14, fill, default");
+		txtCorreoElectronico.setColumns(10);
+		
+		JButton btnActualizar = new JButton("Actualizar");
+		pnlClientesActualizar.add(btnActualizar, "12, 16");
 
 	}
 
