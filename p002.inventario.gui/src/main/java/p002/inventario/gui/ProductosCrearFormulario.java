34ce0068a01b1075ce37c9fbcb4b8cb063d0afd2
infzer0 <johnortizo@outlook.com>
2020-04-26 10:14:56 -0500

    Java - Curso - Event Handler Click para Botón Crear Producto
    Java Curso: 141 Asociar un Event Handler para el Evento Click para el Botón JButton Crear Producto.

--- p002.inventario.gui/src/main/java/p002/inventario/gui/ProductosCrearFormulario.java
+++ p002.inventario.gui/src/main/java/p002/inventario/gui/ProductosCrearFormulario.java
@@ -16,9 +16,12 @@ import modelos.Proveedor;
 
 import com.jgoodies.forms.layout.FormSpecs;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ProductosCrearFormulario extends JInternalFrame {
 
@@ -42,103 +45,104 @@ public class ProductosCrearFormulario extends JInternalFrame {
 	 */
 	public ProductosCrearFormulario(Aplicacion aplicacion) {
 		this.aplicacion = aplicacion;
-		
+
 		setTitle("Productos - Crear");
 		setClosable(true);
 		setBounds(100, 100, 450, 300);
-		
+
 		JPanel pnlProductosCrear = new JPanel();
-		pnlProductosCrear.setBorder(new TitledBorder(null, "Datos", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		pnlProductosCrear
+				.setBorder(new TitledBorder(null, "Datos", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		getContentPane().add(pnlProductosCrear, BorderLayout.CENTER);
-		pnlProductosCrear.setLayout(new FormLayout(new ColumnSpec[] {
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				ColumnSpec.decode("default:grow"),},
-			new RowSpec[] {
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,}));
-		
+		pnlProductosCrear.setLayout(new FormLayout(
+				new ColumnSpec[] { FormSpecs.RELATED_GAP_COLSPEC, FormSpecs.DEFAULT_COLSPEC,
+						FormSpecs.RELATED_GAP_COLSPEC, FormSpecs.DEFAULT_COLSPEC, FormSpecs.RELATED_GAP_COLSPEC,
+						FormSpecs.DEFAULT_COLSPEC, FormSpecs.RELATED_GAP_COLSPEC, FormSpecs.DEFAULT_COLSPEC,
+						FormSpecs.RELATED_GAP_COLSPEC, FormSpecs.DEFAULT_COLSPEC, FormSpecs.RELATED_GAP_COLSPEC,
+						ColumnSpec.decode("default:grow"), },
+				new RowSpec[] { FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC,
+						FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,
+						FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC,
+						FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,
+						FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC,
+						FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, }));
+
 		JLabel lblId = new JLabel("ID");
 		pnlProductosCrear.add(lblId, "2, 2");
-		
+
 		txtId = new JTextField();
 		pnlProductosCrear.add(txtId, "12, 2, fill, default");
 		txtId.setColumns(10);
-		
+
 		JLabel lblNombre = new JLabel("Nombre");
 		pnlProductosCrear.add(lblNombre, "2, 4");
-		
+
 		txtNombre = new JTextField();
 		pnlProductosCrear.add(txtNombre, "12, 4, fill, default");
 		txtNombre.setColumns(10);
-		
+
 		JLabel lblDescripcion = new JLabel("Descripción");
 		pnlProductosCrear.add(lblDescripcion, "2, 6");
-		
+
 		txtDescripcion = new JTextField();
 		pnlProductosCrear.add(txtDescripcion, "12, 6, fill, default");
 		txtDescripcion.setColumns(10);
-		
+
 		JLabel lblPrecioCompra = new JLabel("Precio de compra");
 		pnlProductosCrear.add(lblPrecioCompra, "2, 8");
-		
+
 		txtPrecioCompra = new JTextField();
 		pnlProductosCrear.add(txtPrecioCompra, "12, 8, fill, default");
 		txtPrecioCompra.setColumns(10);
-		
+
 		JLabel lblPrecioVenta = new JLabel("Precio de venta");
 		pnlProductosCrear.add(lblPrecioVenta, "2, 10");
-		
+
 		txtPrecioVenta = new JTextField();
 		pnlProductosCrear.add(txtPrecioVenta, "12, 10, fill, default");
 		txtPrecioVenta.setColumns(10);
-		
+
 		JLabel lblCantidad = new JLabel("Cantidad");
 		pnlProductosCrear.add(lblCantidad, "2, 12");
-		
+
 		txtCantidad = new JTextField();
 		pnlProductosCrear.add(txtCantidad, "12, 12, fill, default");
 		txtCantidad.setColumns(10);
-		
+
 		JLabel lblCantidadMinimaStock = new JLabel("Cantidad mínima stock");
 		pnlProductosCrear.add(lblCantidadMinimaStock, "2, 14");
-		
+
 		txtCantidadMinimaStock = new JTextField();
 		pnlProductosCrear.add(txtCantidadMinimaStock, "12, 14, fill, default");
 		txtCantidadMinimaStock.setColumns(10);
-		
+
 		JLabel lblIdProveedor = new JLabel("Proveedor");
 		pnlProductosCrear.add(lblIdProveedor, "2, 16");
-		
+
 		cbxIdProveedor = new JComboBox();
 		pnlProductosCrear.add(cbxIdProveedor, "12, 16, fill, default");
-		
+
 		JButton btnCrear = new JButton("Crear");
+		btnCrear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String id = txtId.getText().trim();
+				String nombre = txtNombre.getText().trim();
+				String descripcion = txtDescripcion.getText().trim();
+				String precioCompra = txtPrecioCompra.getText().trim();
+				String precioVenta = txtPrecioVenta.getText().trim();
+				String cantidad = txtCantidad.getText().trim();
+				String cantidadMinimaStock = txtCantidadMinimaStock.getText().trim();
+				String idProveedor = cbxIdProveedor.getSelectedItem().toString();
+
+				if (id.isEmpty()) {
+					JOptionPane.showMessageDialog(ProductosCrearFormulario.this, "El campo ID es obligatorio.",
+							"Mensaje", JOptionPane.WARNING_MESSAGE);
+					return;
+				}
+				
+				
+			}
+		});
 		pnlProductosCrear.add(btnCrear, "12, 18");
 
 		cargarDatosProveedores();
@@ -149,12 +153,12 @@ public class ProductosCrearFormulario extends JInternalFrame {
 		idsProveedores = new HashMap<>();
 		String llave;
 		long valor;
-		
+
 		for (Proveedor proveedor : proveedores) {
-			llave = String.format("%d - %s", proveedor.getId(),  proveedor.getNombre());
+			llave = String.format("%d - %s", proveedor.getId(), proveedor.getNombre());
 			valor = proveedor.getId();
 			idsProveedores.put(llave, valor);
-			
+
 			cbxIdProveedor.addItem(llave);
 		}
 	}
