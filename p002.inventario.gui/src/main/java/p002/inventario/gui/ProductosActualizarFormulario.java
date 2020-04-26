c54e531042f2596ec1ac6c85a4e1d2917c227a66
infzer0 <johnortizo@outlook.com>
2020-04-26 16:38:33 -0500

    Java - Curso - ActionListener
    Java Curso: 151 Asociar el Evento de Click para el Botón de Búsqueda de Proveedor - ActionListener.

--- p002.inventario.gui/src/main/java/p002/inventario/gui/ProductosActualizarFormulario.java
+++ p002.inventario.gui/src/main/java/p002/inventario/gui/ProductosActualizarFormulario.java
@@ -4,18 +4,29 @@ import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.border.TitledBorder;
+
+import org.apache.commons.validator.routines.LongValidator;
+
 import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.RowSpec;
+
+import modelos.Producto;
+import modelos.Proveedor;
+
 import com.jgoodies.forms.layout.FormSpecs;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
+import java.awt.event.ActionEvent;
 
 public class ProductosActualizarFormulario extends JInternalFrame {
 
-	
 	/**
 	 * Serial Version ID.
 	 */
@@ -27,122 +38,175 @@ public class ProductosActualizarFormulario extends JInternalFrame {
 	private JTextField txtPrecioVenta;
 	private JTextField txtCantidad;
 	private JTextField txtCantidadMinimaStock;
-
+	private JComboBox cbxIdProveedor;
+	
+	private Aplicacion aplicacion;
+	Map<String, Long> idsProveedores;
+	
 	/**
 	 * Create the frame.
 	 */
-	public ProductosActualizarFormulario() {
+	public ProductosActualizarFormulario(Aplicacion aplicacion) {
+		this.aplicacion = aplicacion;
+		idsProveedores = new HashMap<>();
+		
 		setTitle("Productos - Actualizar");
 		setClosable(true);
 		setBounds(100, 100, 450, 340);
-		
+
 		JPanel pnlProductosActualizar = new JPanel();
-		pnlProductosActualizar.setBorder(new TitledBorder(null, "Datos", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		pnlProductosActualizar
+				.setBorder(new TitledBorder(null, "Datos", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		getContentPane().add(pnlProductosActualizar, BorderLayout.CENTER);
-		pnlProductosActualizar.setLayout(new FormLayout(new ColumnSpec[] {
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				ColumnSpec.decode("default:grow"),},
-			new RowSpec[] {
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,}));
-		
+		pnlProductosActualizar.setLayout(new FormLayout(
+				new ColumnSpec[] { FormSpecs.RELATED_GAP_COLSPEC, FormSpecs.DEFAULT_COLSPEC,
+						FormSpecs.RELATED_GAP_COLSPEC, FormSpecs.DEFAULT_COLSPEC, FormSpecs.RELATED_GAP_COLSPEC,
+						FormSpecs.DEFAULT_COLSPEC, FormSpecs.RELATED_GAP_COLSPEC, FormSpecs.DEFAULT_COLSPEC,
+						FormSpecs.RELATED_GAP_COLSPEC, FormSpecs.DEFAULT_COLSPEC, FormSpecs.RELATED_GAP_COLSPEC,
+						ColumnSpec.decode("default:grow"), },
+				new RowSpec[] { FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC,
+						FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,
+						FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC,
+						FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,
+						FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC,
+						FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,
+						FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, }));
+
 		JLabel lblId = new JLabel("ID");
 		pnlProductosActualizar.add(lblId, "2, 2");
-		
+
 		txtId = new JTextField();
 		pnlProductosActualizar.add(txtId, "12, 2, fill, default");
 		txtId.setColumns(10);
-		
+
 		JButton btnBuscar = new JButton("Buscar");
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String id = txtId.getText().trim();
+
+				if (id.isEmpty()) {
+					JOptionPane.showMessageDialog(ProductosActualizarFormulario.this, "El campo ID es obligatorio.",
+							"Mensaje", JOptionPane.WARNING_MESSAGE);
+					return;
+				}
+
+				Long numero = LongValidator.getInstance().validate(id);
+
+				if (numero == null) {
+					JOptionPane.showMessageDialog(ProductosActualizarFormulario.this, "El campo ID debe ser un número.",
+							"Mensaje", JOptionPane.WARNING_MESSAGE);
+					return;
+				}
+
+				if (numero <= 0) {
+					JOptionPane.showMessageDialog(ProductosActualizarFormulario.this,
+							"El campo ID debe ser un número positivo (mayor a cero).", "Mensaje",
+							JOptionPane.WARNING_MESSAGE);
+					return;
+				}
+
+				Producto producto = aplicacion.buscarProductoPorId(numero.intValue());
+
+				if (producto == null) {
+					JOptionPane.showMessageDialog(ProductosActualizarFormulario.this,
+							"No existe un producto con el ID especificado.", "Mensaje", JOptionPane.WARNING_MESSAGE);
+					txtNombre.setText("");
+					txtDescripcion.setText("");
+					txtPrecioCompra.setText("");
+					txtPrecioVenta.setText("");
+					txtCantidad.setText("");
+					txtCantidadMinimaStock.setText("");
+					idsProveedores.clear();
+
+					return;
+				}
+
+				txtNombre.setText(producto.getNombre());
+				txtDescripcion.setText(producto.getDescripcion());
+				txtPrecioCompra.setText(String.valueOf(producto.getPrecioCompra()));
+				txtPrecioVenta.setText(String.valueOf(producto.getPrecioVenta()));
+				txtCantidad.setText(String.valueOf(producto.getCantidad()));
+				txtCantidadMinimaStock.setText(String.valueOf(producto.getCantidadMinimaStock()));
+				
+				
+				cargarDatosProveedores();
+			}
+		});
 		pnlProductosActualizar.add(btnBuscar, "12, 4");
-		
+
 		JLabel lblNombre = new JLabel("Nombre");
 		pnlProductosActualizar.add(lblNombre, "2, 6");
-		
+
 		txtNombre = new JTextField();
 		txtNombre.setEnabled(false);
 		pnlProductosActualizar.add(txtNombre, "12, 6, fill, default");
 		txtNombre.setColumns(10);
-		
+
 		JLabel lblDescripcion = new JLabel("Descripción");
 		pnlProductosActualizar.add(lblDescripcion, "2, 8");
-		
+
 		txtDescripcion = new JTextField();
 		txtDescripcion.setEnabled(false);
 		pnlProductosActualizar.add(txtDescripcion, "12, 8, fill, default");
 		txtDescripcion.setColumns(10);
-		
+
 		JLabel lblPrecioCompra = new JLabel("Precio Compra");
 		pnlProductosActualizar.add(lblPrecioCompra, "2, 10");
-		
+
 		txtPrecioCompra = new JTextField();
 		txtPrecioCompra.setEnabled(false);
 		pnlProductosActualizar.add(txtPrecioCompra, "12, 10, fill, default");
 		txtPrecioCompra.setColumns(10);
-		
+
 		JLabel lblPrecioVenta = new JLabel("Precio Venta");
 		pnlProductosActualizar.add(lblPrecioVenta, "2, 12");
-		
+
 		txtPrecioVenta = new JTextField();
 		txtPrecioVenta.setEnabled(false);
 		pnlProductosActualizar.add(txtPrecioVenta, "12, 12, fill, default");
 		txtPrecioVenta.setColumns(10);
-		
+
 		JLabel lblCantidad = new JLabel("Cantidad");
 		pnlProductosActualizar.add(lblCantidad, "2, 14");
-		
+
 		txtCantidad = new JTextField();
 		txtCantidad.setEnabled(false);
 		pnlProductosActualizar.add(txtCantidad, "12, 14, fill, default");
 		txtCantidad.setColumns(10);
-		
+
 		JLabel lblCantidadMinimaStock = new JLabel("Cantidad Mínima Stock");
 		pnlProductosActualizar.add(lblCantidadMinimaStock, "2, 16");
-		
+
 		txtCantidadMinimaStock = new JTextField();
 		txtCantidadMinimaStock.setEnabled(false);
 		pnlProductosActualizar.add(txtCantidadMinimaStock, "12, 16, fill, default");
 		txtCantidadMinimaStock.setColumns(10);
-		
+
 		JLabel lblIdProveedor = new JLabel("ID Proveedor");
 		pnlProductosActualizar.add(lblIdProveedor, "2, 18");
-		
-		JComboBox cbxIdProveedor = new JComboBox();
+
+		cbxIdProveedor = new JComboBox();
 		cbxIdProveedor.setEnabled(false);
 		pnlProductosActualizar.add(cbxIdProveedor, "12, 18, fill, default");
-		
+
 		JButton btnActualizar = new JButton("Actualizar");
 		btnActualizar.setEnabled(false);
 		pnlProductosActualizar.add(btnActualizar, "12, 20");
 
 	}
 
+	private void cargarDatosProveedores() {
+		Proveedor[] proveedores = aplicacion.obtenerProveedores();
+		idsProveedores.clear();
+		String llave;
+		long valor;
+
+		for (Proveedor proveedor : proveedores) {
+			llave = String.format("%d - %s", proveedor.getId(), proveedor.getNombre());
+			valor = proveedor.getId();
+			idsProveedores.put(llave, valor);
+
+			cbxIdProveedor.addItem(llave);
+		}
+	}
 }
