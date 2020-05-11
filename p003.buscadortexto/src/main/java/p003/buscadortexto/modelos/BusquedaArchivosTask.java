eb2550995ba17064a29cc3a5291faadd7c237ca1
infzer0 <johnortizo@outlook.com>
2020-05-11 08:21:19 -0500

    Java Curso - Apertura de Archivo con java.util.Scanner
    Java Curso: 198 Inicio de Implementación del Método doInBackground() y Apertura Archivo con Scanner.

--- p003.buscadortexto/src/main/java/p003/buscadortexto/modelos/BusquedaArchivosTask.java
+++ p003.buscadortexto/src/main/java/p003/buscadortexto/modelos/BusquedaArchivosTask.java
@@ -1,7 +1,9 @@
 package p003.buscadortexto.modelos;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Scanner;
 
 import javax.swing.SwingWorker;
 import javax.swing.table.DefaultTableModel;
@@ -10,16 +12,39 @@ public class BusquedaArchivosTask extends SwingWorker<Void, Void> {
 	
 	private List<File> archivosSeleccionados;
 	private DefaultTableModel dtmResultados;
+	private String texto;
+	private boolean buscarPorExpresionRegular;
 
-	public BusquedaArchivosTask(List<File> archivosSeleccionados, DefaultTableModel dtmResultados) {
+	public BusquedaArchivosTask(List<File> archivosSeleccionados, DefaultTableModel dtmResultados, String texto,
+			boolean buscarPorExpresionRegular) {
 		this.archivosSeleccionados = archivosSeleccionados;
 		this.dtmResultados = dtmResultados;
+		this.texto = texto;
+		this.buscarPorExpresionRegular = buscarPorExpresionRegular;
 	}
 
 	@Override
 	protected Void doInBackground() throws Exception {
-		// TODO Auto-generated method stub
+		for (File archivo : archivosSeleccionados) {
+			buscarContenido(archivo);
+		}
+		
 		return null;
+	}
+
+	private void buscarContenido(File archivo) {
+		try {
+			Scanner lector = new Scanner(archivo, "UTF-8");
+			String linea;
+			
+			while(lector.hasNextLine()) {
+				linea = lector.nextLine();
+				
+				
+			}
+		} catch(FileNotFoundException e) {
+			
+		}
 	}
 
 }
