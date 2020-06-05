47c3dd944032db3fe7935315c07be909c3e4cf34
infzer0 <johnortizo@outlook.com>
2020-06-05 11:58:47 -0500

    Java Curso: 260 Distribución de los Componentes Integrales de la Interfaz Gráfica de Usuario (Layout)
    Distribución de los Componentes Integrales de la Interfaz Gráfica de Usuario (Layout) con el lenguaje de programación Java.

--- p005.redimensionarimagenes/src/main/java/gui/VentanaPrincipal.java
+++ p005.redimensionarimagenes/src/main/java/gui/VentanaPrincipal.java
@@ -1,12 +1,15 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 
 public class VentanaPrincipal extends JFrame {
 
@@ -44,7 +47,18 @@ public class VentanaPrincipal extends JFrame {
 		pnlPrincipal = new JPanel();
 		pnlPrincipal.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(pnlPrincipal);
-		pnlPrincipal.setLayout(new BoxLayout(pnlPrincipal, BoxLayout.X_AXIS));
+		pnlPrincipal.setLayout(new BoxLayout(pnlPrincipal, BoxLayout.Y_AXIS));
+		
+		JLabel lblImagen = new JLabel("");
+		lblImagen.setSize(350, 500);
+		lblImagen.setMaximumSize(new Dimension(400, 450));
+		lblImagen.setPreferredSize(new Dimension(400, 450));
+		lblImagen.setMaximumSize(new Dimension(400, 450));
+		lblImagen.setAlignmentX(Component.CENTER_ALIGNMENT);
+		pnlPrincipal.add(lblImagen);
+		
+		JPanel pnlBotones = new JPanel();
+		pnlPrincipal.add(pnlBotones);
 	}
 
 }
