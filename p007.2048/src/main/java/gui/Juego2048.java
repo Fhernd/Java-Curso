8f02ea3d00f3fe9114c5379e43371d6046a1c1f5
infzer0 <johnortizo@outlook.com>
2020-09-01 18:43:11 -0500

    Java Curso: 282 Proyecto 7 - Creación de la Clase Juego2048 para Representar el Área de Juego
    Proyecto 7 - Creación de la Clase Juego2048 para Representar el Área de Juego con el lenguaje de programación Java.

---
+++ p007.2048/src/main/java/gui/Juego2048.java
@@ -0,0 +1,12 @@
+package gui;
+
+import javax.swing.JPanel;
+
+public class Juego2048 extends JPanel {
+
+	/**
+	 * Representa el ID de versión de la clase.
+	 */
+	private static final long serialVersionUID = -7189306025861606629L;
+
+}
--- p007.2048/src/main/java/modelos/Baldosa.java
+++ p007.2048/src/main/java/modelos/Baldosa.java
@@ -25,6 +25,24 @@ public class Baldosa {
 				return new Color(ColorBaldosa.COLOR_2.obtenerColorHex());
 			case 4:
 				return new Color(ColorBaldosa.COLOR_4.obtenerColorHex());
+			case 8:
+				return new Color(ColorBaldosa.COLOR_8.obtenerColorHex());
+			case 16:
+				return new Color(ColorBaldosa.COLOR_16.obtenerColorHex());
+			case 32:
+				return new Color(ColorBaldosa.COLOR_32.obtenerColorHex());
+			case 64:
+				return new Color(ColorBaldosa.COLOR_64.obtenerColorHex());
+			case 128:
+				return new Color(ColorBaldosa.COLOR_128.obtenerColorHex());
+			case 256:
+				return new Color(ColorBaldosa.COLOR_256.obtenerColorHex());
+			case 512:
+				return new Color(ColorBaldosa.COLOR_512.obtenerColorHex());
+			case 1024:
+				return new Color(ColorBaldosa.COLOR_1024.obtenerColorHex());
+			case 2048:
+				return new Color(ColorBaldosa.COLOR_2048.obtenerColorHex());
 		}
 		
 		return new Color(ColorBaldosa.COLOR_0.obtenerColorHex());
