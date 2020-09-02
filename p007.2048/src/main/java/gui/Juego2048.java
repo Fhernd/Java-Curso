f637f88a3485b130ac5d3143828d5bd750b3a1fb
infzer0 <johnortizo@outlook.com>
2020-09-02 17:00:45 -0500

    Java Curso: 283 Proyecto 7 - Constantes, Variables de Instancia y Clase Adaptarada para Mover Baldosas
    Proyecto 7 - Constantes, Variables de Instancia y Clase Adaptarada para Mover Baldosas con el lenguaje de programación Java.

--- p007.2048/src/main/java/gui/Juego2048.java
+++ p007.2048/src/main/java/gui/Juego2048.java
@@ -1,6 +1,13 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 import javax.swing.JPanel;
+
+import modelos.Baldosa;
 
 public class Juego2048 extends JPanel {
 
@@ -8,5 +15,56 @@ public class Juego2048 extends JPanel {
 	 * Representa el ID de versión de la clase.
 	 */
 	private static final long serialVersionUID = -7189306025861606629L;
-
+	
+	private static final int TAMAGNIO_BALDOSA = 64;
+	private static final int MARGEN_BALDOSA = 16;
+	private static final Color COLOR_FONDO_PANEL = new Color(0xffffff);
+	
+	private boolean gano;
+	private boolean perdio;
+	private int puntaje;
+	
+	private Baldosa[] baldosas;
+	
+	public Juego2048() {
+		setPreferredSize(new Dimension(350, 400));
+		setFocusable(true);
+		
+		addKeyListener(new ManejadorComandos());
+	}
+	
+	class ManejadorComandos extends KeyAdapter {
+		@Override
+		public void keyPressed(KeyEvent evento) {
+			if (!perdio && !gano) {
+				switch(evento.getKeyCode()) {
+					case KeyEvent.VK_UP:
+						// TODO: Subir las baldosas
+						break;
+					case KeyEvent.VK_RIGHT:
+						// TODO: Mover a la izquierda las baldosas
+						break;
+					case KeyEvent.VK_DOWN:
+						// TODO: Bajas las baldosas
+						break;
+					case KeyEvent.VK_LEFT:
+						// TODO: Mover a la derecha las baldosas
+						break;
+				}
+			}
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
