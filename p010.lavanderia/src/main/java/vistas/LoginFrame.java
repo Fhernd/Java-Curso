60f328cfb97c4292abfb643d8719c418d2be6d2e
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-03 10:52:21 -0500

    Creando el layout del formulario de inicio de sesión
    Creando el layout del formulario de inicio de sesión.

--- p010.lavanderia/src/main/java/vistas/LoginFrame.java
+++ p010.lavanderia/src/main/java/vistas/LoginFrame.java
@@ -3,8 +3,19 @@ package vistas;
 import java.awt.EventQueue;
 
 import javax.swing.JInternalFrame;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
 
 public class LoginFrame extends JInternalFrame {
+
+	/**
+	 * UID de la clase.
+	 */
+	private static final long serialVersionUID = 1L;
 
 	/**
 	 * Launch the application.
@@ -26,7 +37,15 @@ public class LoginFrame extends JInternalFrame {
 	 * Create the frame.
 	 */
 	public LoginFrame() {
+		setTitle("Login");
 		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "Datos", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(new FormLayout(new ColumnSpec[] {},
+			new RowSpec[] {}));
 
 	}
 
