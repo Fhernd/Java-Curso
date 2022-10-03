6062e6bb8908abd21a34418cef77226cdb43ce8c
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-03 10:37:10 -0500

    Métodos para obtener empleados, eliminar por ID un empleado
    Métodos para obtener empleados, eliminar por ID un empleado, crear atención, obtener atenciones por servicio ID.

--- p010.lavanderia/src/main/java/modelos/GestorLavanderia.java
+++ p010.lavanderia/src/main/java/modelos/GestorLavanderia.java
@@ -129,9 +129,10 @@ public class GestorLavanderia {
 
     /**
      * Inicia sesión a partir de las credenciales de un usuario.
+     *
      * @param correo Correo del usuario.
-     * @param clave Clave del usuario.
-     *@return Usuario Usuario encontrado.
+     * @param clave  Clave del usuario.
+     * @return Usuario Usuario encontrado.
      */
     public Usuario iniciarSesion(String correo, String clave) {
         return accesoDatos.iniciarSesion(correo, clave);
@@ -139,6 +140,7 @@ public class GestorLavanderia {
 
     /**
      * Elimina un usuario a partir de su correo.
+     *
      * @param correo Correo del usuario.
      * @return true si se eliminó correctamente, false en caso contrario.
      */
@@ -148,6 +150,7 @@ public class GestorLavanderia {
 
     /**
      * Crear un nuevo empleado.
+     *
      * @param empleado Empleado a crear.
      * @return Empleado Empleado creado.
      */
@@ -157,6 +160,7 @@ public class GestorLavanderia {
 
     /**
      * Modifica los datos de un empleado.
+     *
      * @param empleado Empleado a modificar.
      * @return true si se modificó correctamente, false en caso contrario.
      */
@@ -166,9 +170,50 @@ public class GestorLavanderia {
 
     /**
      * Recupera todos los empleados.
+     *
      * @return List<Empleado> Lista de empleados.
      */
     public List<Empleado> obtenerEmpleados() {
         return accesoDatos.obtenerEmpleados();
+    }
+
+    /**
+     * Elimina un empleado a partir de su ID.
+     *
+     * @param id ID del empleado.
+     * @return true si se eliminó correctamente, false en caso contrario.
+     */
+    public boolean eliminarEmpleadoPorId(int id) {
+        return accesoDatos.eliminarEmpleadoPorId(id);
+    }
+
+    /**
+     * Crea una nueva atención.
+     *
+     * @param atencion Atención a crear.
+     * @return Atencion Atención creada.
+     */
+    public Atencion crearAtencion(Atencion atencion) {
+        return accesoDatos.crearAtencion(atencion);
+    }
+
+    /**
+     * Lista atenciones a partir del ID de un servicio.
+     *
+     * @param servicioId ID del servicio.
+     * @return List<Atencion> Lista de atenciones.
+     */
+    public List<Atencion> obtenerAtencionesPorServicioId(int servicioId) {
+        return accesoDatos.obtenerAtencionesPorServicioId(servicioId);
+    }
+
+    /**
+     * Listar atenciones a partir del ID de un tipo de atención.
+     *
+     * @param tipoAtencionId ID del tipo de atención.
+     * @return List<Atencion> Lista de atenciones.
+     */
+    public List<Atencion> obtenerAtencionesPorTipoAtencionId(int tipoAtencionId) {
+        return accesoDatos.obtenerAtencionesPorTipoAtencionId(tipoAtencionId);
     }
 }
