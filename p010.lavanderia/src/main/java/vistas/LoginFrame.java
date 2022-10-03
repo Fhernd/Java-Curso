bbe171ed58fc267d1bc234a4a815f5bf1d2fe327
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-03 16:31:08 -0500

    Diseño del formulario de inicio de sesión completado
    Diseño del formulario de inicio de sesión completado.

--- p010.lavanderia/src/main/java/vistas/LoginFrame.java
+++ p010.lavanderia/src/main/java/vistas/LoginFrame.java
@@ -9,6 +9,11 @@ import com.jgoodies.forms.layout.RowSpec;
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
+import com.jgoodies.forms.layout.FormSpecs;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
 
 public class LoginFrame extends JInternalFrame {
 
@@ -16,6 +21,8 @@ public class LoginFrame extends JInternalFrame {
 	 * UID de la clase.
 	 */
 	private static final long serialVersionUID = 1L;
+	private JTextField txtEmail;
+	private JPasswordField txtPassword;
 
 	/**
 	 * Launch the application.
@@ -38,14 +45,44 @@ public class LoginFrame extends JInternalFrame {
 	 */
 	public LoginFrame() {
 		setTitle("Login");
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 170);
 		getContentPane().setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel = new JPanel();
 		panel.setBorder(new TitledBorder(null, "Datos", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		getContentPane().add(panel, BorderLayout.CENTER);
-		panel.setLayout(new FormLayout(new ColumnSpec[] {},
-			new RowSpec[] {}));
+		panel.setLayout(new FormLayout(new ColumnSpec[] {
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),},
+			new RowSpec[] {
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblEmail = new JLabel("Email:");
+		panel.add(lblEmail, "2, 2");
+		
+		txtEmail = new JTextField();
+		panel.add(txtEmail, "8, 2, fill, default");
+		txtEmail.setColumns(10);
+		
+		JLabel lblPassword = new JLabel("Contraseña:");
+		panel.add(lblPassword, "2, 4");
+		
+		txtPassword = new JPasswordField();
+		panel.add(txtPassword, "8, 4, fill, default");
+		
+		JButton btnIniciarSesion = new JButton("Iniciar sesión");
+		panel.add(btnIniciarSesion, "8, 6");
 
 	}
 
