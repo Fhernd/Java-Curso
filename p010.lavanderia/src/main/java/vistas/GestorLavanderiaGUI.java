1984e40684f487aea171e5a1926e920458d6fc11
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-03 16:54:15 -0500

    Escritorio virtual para ventanas de operaciones y actualización de MySQL
    Escritorio virtual para ventanas de operaciones y actualización de MySQL.

--- p010.lavanderia/src/main/java/vistas/GestorLavanderiaGUI.java
+++ p010.lavanderia/src/main/java/vistas/GestorLavanderiaGUI.java
@@ -2,7 +2,10 @@ package vistas;
 
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class GestorLavanderiaGUI {
 
@@ -37,8 +40,31 @@ public class GestorLavanderiaGUI {
 	private void initialize() {
 		frmGestorLavanderiaGUI = new JFrame();
 		frmGestorLavanderiaGUI.setTitle("Gestor Lavandería");
-		frmGestorLavanderiaGUI.setBounds(100, 100, 450, 300);
+		frmGestorLavanderiaGUI.setBounds(100, 100, 800, 600);
 		frmGestorLavanderiaGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmGestorLavanderiaGUI.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JDesktopPane desktopPane = new JDesktopPane();
+		frmGestorLavanderiaGUI.getContentPane().add(desktopPane, BorderLayout.CENTER);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frmGestorLavanderiaGUI.setJMenuBar(menuBar);
+		
+		JMenu mnuArchivo = new JMenu("Archivo");
+		menuBar.add(mnuArchivo);
+		
+		JMenuItem mniIniciarSesion = new JMenuItem("Iniciar sesión...");
+		mnuArchivo.add(mniIniciarSesion);
+		
+		JMenuItem mniSalir = new JMenuItem("Salir");
+		mniSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int opcion = JOptionPane.showConfirmDialog(frmGestorLavanderiaGUI, "¿Está seguro que desea salir?", "Salir", JOptionPane.YES_NO_OPTION);
+				if (opcion == JOptionPane.YES_OPTION) {
+					System.exit(0);
+				}
+			}
+		});
+		mnuArchivo.add(mniSalir);
 	}
-
 }
