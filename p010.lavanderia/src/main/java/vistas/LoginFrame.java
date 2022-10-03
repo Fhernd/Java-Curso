5f076ea3c2f669484b376ddf4ab5cb41a05a10f9
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-03 10:46:46 -0500

    Frame para el diálogo de inicio de sesión
    Frame para el diálogo de inicio de sesión.

---
+++ p010.lavanderia/src/main/java/vistas/LoginFrame.java
@@ -0,0 +1,33 @@
+package vistas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JInternalFrame;
+
+public class LoginFrame extends JInternalFrame {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginFrame frame = new LoginFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LoginFrame() {
+		setBounds(100, 100, 450, 300);
+
+	}
+
+}
