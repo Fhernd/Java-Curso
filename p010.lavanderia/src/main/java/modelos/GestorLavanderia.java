32f57879d69e458c5794fe7f9202a99632d48008
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-03 08:52:04 -0500

    Métodos para acceder a roles, tipos de documentos y tipos de atención
    Métodos para acceder a roles, tipos de documentos y tipos de atención.

--- p010.lavanderia/src/main/java/modelos/GestorLavanderia.java
+++ p010.lavanderia/src/main/java/modelos/GestorLavanderia.java
@@ -1,4 +1,54 @@
 package modelos;
 
+import datos.AccesoDatos;
+
+import java.util.List;
+
 public class GestorLavanderia {
+    private AccesoDatos accesoDatos;
+
+    public GestorLavanderia() {
+        accesoDatos = new AccesoDatos();
+    }
+
+    /**
+     * Obtiene el listado de roles.
+     * @return Listado de roles.
+     */
+    public List<Rol> getRoles() {
+        return accesoDatos.obtenerRoles();
+    }
+
+    /**
+     * Obtiene el listado de tipos de documento.
+     * @return Listado de tipos de documento.
+     */
+    public List<TipoDocumento> obtenerTiposDocumentos() {
+        return accesoDatos.obtenerTiposDocumentos();
+    }
+
+    /**
+     * Obtiene todos los tipos de atencion.
+     * @return Lista de tipos de atención.
+     */
+    public List<TipoAtencion> obtenerTiposAtencion() {
+        return accesoDatos.obtenerTiposAtencion();
+    }
+
+    /**
+     * Crea una nueva dirección.
+     * @param direccion Direccion a crear.
+     */
+    public Direccion crearDireccion(Direccion direccion) {
+        return accesoDatos.crearDireccion(direccion);
+    }
+
+    /**
+     * Obtener una dirección a partir del ID del cliente.
+     * @param clienteId ID del cliente.
+     * @return Direccion Dirección encontrada.
+     */
+    public Direccion obtenerDireccionPorClientId(int clienteId) {
+        return accesoDatos.obtenerDireccionPorClientId(clienteId);
+    }
 }
