cf841a415489f19b1a15f9f250f416473c26d310
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-04 11:40:34 -0500

    Se ha creado la clase para el formulario de crear empleado
    Se ha creado la clase para el formulario de crear empleado.

---
+++ p010.lavanderia/src/main/java/vistas/EmpleadoCrearFrame.java
@@ -0,0 +1,20 @@
+package vistas;
+
+import javax.swing.JInternalFrame;
+
+public class EmpleadoCrearFrame extends JInternalFrame {
+
+	/**
+	 * ID de clase.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the frame.
+	 */
+	public EmpleadoCrearFrame() {
+		setBounds(100, 100, 450, 300);
+
+	}
+
+}
