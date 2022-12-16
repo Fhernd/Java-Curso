e8fe5472cc2a784c3eb51bc62a00f27a0d7f51fb
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-12-16 15:25:06 -0500

    Modelo personalizado para un control JComboBox

---
+++ p010.lavanderia/src/main/java/vistas/models/CustomComboBoxModel.java
@@ -0,0 +1,34 @@
+package vistas.models;
+
+import modelos.TipoDocumento;
+
+import javax.swing.*;
+
+public class CustomComboBoxModel extends AbstractListModel implements ComboBoxModel {
+    TipoDocumento[] tipoDocumentos;
+
+    public CustomComboBoxModel(TipoDocumento[] tipoDocumentos) {
+        this.tipoDocumentos = tipoDocumentos;
+    }
+
+    String selection = null;
+
+    @Override
+    public Object getElementAt(int index) {
+        return tipoDocumentos[index];
+    }
+
+    @Override
+    public int getSize() {
+        return tipoDocumentos.length;
+    }
+
+    public void setSelectedItem(Object anItem) {
+        selection = (String) anItem;
+    }
+
+    @Override
+    public Object getSelectedItem() {
+        return selection; // to add the selection to the combo box
+    }
+}
