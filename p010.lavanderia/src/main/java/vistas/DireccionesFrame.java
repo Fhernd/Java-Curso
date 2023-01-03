09fe2f64b4ed467d72a3b89eb884c0e41ba67d55
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-01-03 15:59:06 -0500

    Creando el formulario para crear direcciones

--- p010.lavanderia/src/main/java/vistas/DireccionesFrame.java
+++ p010.lavanderia/src/main/java/vistas/DireccionesFrame.java
@@ -3,8 +3,20 @@ package vistas;
 import java.awt.EventQueue;
 
 import javax.swing.JInternalFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.border.TitledBorder;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
 
 public class DireccionesFrame extends JInternalFrame {
+	private JTextField txtDescripcion;
 
 	/**
 	 * Launch the application.
@@ -26,7 +38,48 @@ public class DireccionesFrame extends JInternalFrame {
 	 * Create the frame.
 	 */
 	public DireccionesFrame() {
+		setTitle("Direcciones");
+		setMaximizable(true);
+		setClosable(true);
 		setBounds(100, 100, 450, 300);
+		
+		JPanel pnlDatos = new JPanel();
+		pnlDatos.setBorder(new TitledBorder(null, "Datos", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		getContentPane().add(pnlDatos, BorderLayout.NORTH);
+		pnlDatos.setLayout(new FormLayout(new ColumnSpec[] {
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),},
+			new RowSpec[] {
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblDescripcion = new JLabel("Dirección:");
+		pnlDatos.add(lblDescripcion, "2, 2");
+		
+		txtDescripcion = new JTextField();
+		pnlDatos.add(txtDescripcion, "10, 2, fill, default");
+		txtDescripcion.setColumns(10);
+		
+		JLabel lblClienteId = new JLabel("Cliente:");
+		pnlDatos.add(lblClienteId, "2, 4");
+		
+		JComboBox cbxClienteId = new JComboBox();
+		pnlDatos.add(cbxClienteId, "10, 4, fill, default");
+		
+		JButton btnBuscarClientePordocumento = new JButton("Buscar cliente por documento...");
+		pnlDatos.add(btnBuscarClientePordocumento, "10, 6, default, bottom");
 
 	}
 
