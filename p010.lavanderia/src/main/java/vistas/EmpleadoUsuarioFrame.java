4d7157c3ce42c65e5266cd854c573e32558bf371
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-01-07 14:27:26 -0500

    JInternalFrame para manipular datos de empleados y usuarios

---
+++ p010.lavanderia/src/main/java/vistas/EmpleadoUsuarioFrame.java
@@ -0,0 +1,17 @@
+package vistas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JInternalFrame;
+
+public class EmpleadoUsuarioFrame extends JInternalFrame {
+
+	/**
+	 * Create the frame.
+	 */
+	public EmpleadoUsuarioFrame() {
+		setBounds(100, 100, 450, 300);
+
+	}
+
+}
