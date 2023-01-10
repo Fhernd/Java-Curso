0a056dcaa48ba7a0a14ecd48fb6e7b830ed6d293
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-01-10 15:12:04 -0500

    Representación textual de un objeto Rol

--- p010.lavanderia/src/main/java/modelos/Rol.java
+++ p010.lavanderia/src/main/java/modelos/Rol.java
@@ -27,4 +27,9 @@ public class Rol {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
 }
