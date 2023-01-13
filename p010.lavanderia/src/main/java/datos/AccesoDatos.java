0c7659088b2708b92a2c3be45956827865618d14
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-01-13 16:32:35 -0500

    Función para actualizar un empleado

--- p010.lavanderia/src/main/java/datos/AccesoDatos.java
+++ p010.lavanderia/src/main/java/datos/AccesoDatos.java
@@ -1192,4 +1192,28 @@ public class AccesoDatos {
 
         return empleados;
     }
+
+    /**
+     * Actualizar los datos de un empleado.
+     *
+     * @param empleado Empleado.
+     * @return boolean Resultado de la operación.
+     */
+    public boolean actualizarEmpleado(Empleado empleado) {
+        try {
+            final String SQL = "UPDATE empleado SET nombres = ?, apellidos = ?, sueldo = ?, rol_id = ? WHERE id = ?";
+            PreparedStatement sentencia = conexion.getConnection().prepareStatement(SQL);
+            sentencia.setString(1, empleado.getNombres());
+            sentencia.setString(2, empleado.getApellidos());
+            sentencia.setDouble(3, empleado.getSueldo());
+            sentencia.setInt(4, empleado.getRolId());
+            sentencia.setInt(5, empleado.getId());
+
+            return sentencia.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+        }
+
+        return false;
+    }
 }
