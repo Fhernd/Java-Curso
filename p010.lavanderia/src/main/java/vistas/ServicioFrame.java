50771809ba71a9915a6d930cf76350a9fa71039a
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-01-18 11:06:11 -0500

    Inicio creación de la vista de servicios

---
+++ p010.lavanderia/src/main/java/vistas/ServicioFrame.java
@@ -0,0 +1,17 @@
+package vistas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JInternalFrame;
+
+public class ServicioFrame extends JInternalFrame {
+
+	/**
+	 * Create the frame.
+	 */
+	public ServicioFrame() {
+		setBounds(100, 100, 450, 300);
+
+	}
+
+}
