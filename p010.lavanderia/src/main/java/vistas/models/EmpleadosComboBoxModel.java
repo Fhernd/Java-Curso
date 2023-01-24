288df341768a7574b80c5ff0a3f676c2ac27ddf0
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-01-24 16:36:52 -0500

    Búsqueda del índice donde encuentra el empleado

--- p010.lavanderia/src/main/java/vistas/models/EmpleadosComboBoxModel.java
+++ p010.lavanderia/src/main/java/vistas/models/EmpleadosComboBoxModel.java
@@ -31,4 +31,19 @@ public class EmpleadosComboBoxModel extends AbstractListModel implements ComboBo
     public Object getElementAt(int index) {
         return empleados[index];
     }
+
+    /**
+     * Devuelve el índice del elemento con el ID indicado.
+     *
+     * @param id ID del elemento a buscar.
+     * @return Índice del elemento con el ID indicado, o -1 si no se encuentra.
+     */
+    public int getIndexOf(int id) {
+        for (int i = 0; i < empleados.length; i++) {
+            if (empleados[i].getId() == id) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
