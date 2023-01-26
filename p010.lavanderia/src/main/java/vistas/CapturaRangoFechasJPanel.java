3d8fba29e6e306ac3397b9f9648ef0733f0ca2d1
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-01-26 15:04:27 -0500

    Inicio del diseño del panel de captura de un rango de fechas

---
+++ p010.lavanderia/src/main/java/vistas/CapturaRangoFechasJPanel.java
@@ -0,0 +1,24 @@
+package vistas;
+
+import javax.swing.JPanel;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+
+public class CapturaRangoFechasJPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2309572527450541301L;
+
+	/**
+	 * Create the panel.
+	 */
+	public CapturaRangoFechasJPanel() {
+		setLayout(new FormLayout(new ColumnSpec[] {},
+			new RowSpec[] {}));
+
+	}
+
+}
