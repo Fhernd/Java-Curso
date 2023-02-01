70eb57f999ac265354a6883cd6a78b8e01a73600
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-02-01 11:02:11 -0500

    Creada la ventana para editar y crear tipos de atención

---
+++ p010.lavanderia/src/main/java/vistas/TipoAtencionFrame.java
@@ -0,0 +1,20 @@
+package vistas;
+
+import javax.swing.JInternalFrame;
+
+public class TipoAtencionFrame extends JInternalFrame {
+
+	/**
+	 * Serial version UID.
+	 */
+	private static final long serialVersionUID = 2632599037283355071L;
+
+	/**
+	 * Create the frame.
+	 */
+	public TipoAtencionFrame() {
+		setBounds(100, 100, 450, 300);
+
+	}
+
+}
