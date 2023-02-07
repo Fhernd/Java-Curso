7b07cc9798a4c5e50d167bd822fdebc8a89a0589
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-02-07 11:59:21 -0500

    Código inicial para la ventana de generación de reportes

---
+++ p010.lavanderia/src/main/java/vistas/GeneracionReportesFrame.java
@@ -0,0 +1,20 @@
+package vistas;
+
+import javax.swing.JInternalFrame;
+
+public class GeneracionReportesFrame extends JInternalFrame {
+
+	/**
+	 * Serial version ID.
+	 */
+	private static final long serialVersionUID = 8174451787899692365L;
+
+	/**
+	 * Create the frame.
+	 */
+	public GeneracionReportesFrame() {
+		setBounds(100, 100, 450, 300);
+
+	}
+
+}
