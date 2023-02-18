184e70fcb3b59115eb8c6870a671469b9f83f894
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-02-18 10:07:58 -0500

    Leyendo credenciales de acceso a la base de datos desde variables de entorno

--- p010.lavanderia/src/main/java/datos/Conexion.java
+++ p010.lavanderia/src/main/java/datos/Conexion.java
@@ -4,11 +4,12 @@ import java.sql.*;
 
 public class Conexion {
 
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/lavanderia?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
-    private static final String JDBC_USER = "root";
-    private static final String JDBC_PASSWORD = "";
-
     public static Connection getConnection() throws SQLException {
+        final String JDBC_URL = "jdbc:mysql://localhost:3306/" + System.getenv("GESTOR_LAVANDERIA_JDBC_DATABASE") + "?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        final String JDBC_USER = System.getenv("GESTOR_LAVANDERIA_JDBC_USER");
+        System.out.println("JDBC_URL: " + JDBC_URL);
+        System.out.println("JDBC_USER: " + JDBC_USER);
+        String JDBC_PASSWORD = "";
         return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
     }
 
