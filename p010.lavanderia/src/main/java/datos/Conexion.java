7994243cb17a958cfa25079be5b21839dd23a90b
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-02-18 10:11:34 -0500

    Intento de lectura de la contraseña del usuario de la base de datos

--- p010.lavanderia/src/main/java/datos/Conexion.java
+++ p010.lavanderia/src/main/java/datos/Conexion.java
@@ -7,9 +7,13 @@ public class Conexion {
     public static Connection getConnection() throws SQLException {
         final String JDBC_URL = "jdbc:mysql://localhost:3306/" + System.getenv("GESTOR_LAVANDERIA_JDBC_DATABASE") + "?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
         final String JDBC_USER = System.getenv("GESTOR_LAVANDERIA_JDBC_USER");
-        System.out.println("JDBC_URL: " + JDBC_URL);
-        System.out.println("JDBC_USER: " + JDBC_USER);
-        String JDBC_PASSWORD = "";
+
+        String JDBC_PASSWORD = System.getenv("GESTOR_LAVANDERIA_JDBC_PASSWORD");
+
+        if (JDBC_PASSWORD == null) {
+            JDBC_PASSWORD = "";
+        }
+
         return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
     }
 
